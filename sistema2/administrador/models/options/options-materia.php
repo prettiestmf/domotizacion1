2932a2df3cab692fd395c78152c80c9dd23d128f
prettiestmf <2190038@ipisa.edu.do>
2023-05-04 16:29:44 -0400

    qwertyuiop

--- sistema2/administrador/models/options/options-materia.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once '../../../includes/conexion.php';
-
-$sql = "SELECT materia_id,nombre_materia FROM materias WHERE estado = 1";
-$query = $pdo->prepare($sql);
-$query->execute();
-$data = $query->fetchAll(PDO::FETCH_ASSOC);
-echo json_encode($data,JSON_UNESCAPED_UNICODE);
